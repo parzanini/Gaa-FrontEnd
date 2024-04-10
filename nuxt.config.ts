2c5dda0b928a316e945c8d8c2f926705d4d9f0b8
Thiago Gomes Parzanini <parzanini@hotmail.com>
2024-04-10 10:27:40 +0100

    dev config off

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from 'path';
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['nuxt-swiper', '@pinia/nuxt'],
   alias: {
     '@': resolve(__dirname, '/'),
